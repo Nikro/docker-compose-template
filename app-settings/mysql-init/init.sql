b5225f9f437a3d4c37ec557891750f939dff2c01
Sergiu Nagailic <nikro.md@gmail.com>
2023-05-22 19:07:34 +0200

    No need for init for sql.

--- app-settings/mysql-init/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS your_database;
-CREATE USER IF NOT EXISTS 'your_user'@'%' IDENTIFIED BY 'your_password';
-GRANT ALL PRIVILEGES ON your_database.* TO 'your_user'@'%';
-FLUSH PRIVILEGES;
